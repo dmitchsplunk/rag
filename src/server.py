344f4591d606146ea37eb94bfb67a0941a7fbc38
dmitchsplunk <derekmitchell@splunk.com>
2025-05-09 14:37:46 -0700

    added opentelemetry instrumentation

--- src/server.py
+++ src/server.py
@@ -45,8 +45,13 @@ from pymilvus.exceptions import MilvusException
 from pymilvus.exceptions import MilvusUnavailableException
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
+import openlit
+
 logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO').upper())
 logger = logging.getLogger(__name__)
+
+# Initialize OpenLIT instrumentation
+openlit.init()
 
 tags_metadata = [
     {
